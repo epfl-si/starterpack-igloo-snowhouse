76f07ac408d6aa3759d27928dcbe251dd1ba7c94
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2023-08-17 10:30:55 +0200

    [rewrite] `codewars.cpp` from an actual kata
    Specifically, [this one](https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/cpp)

--- codewars.cpp
+++ codewars.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <igloo/igloo_alt.h>
+using namespace igloo;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+bool is_square(int n) {
+    return true;
+}
+
+Describe(is_square_function)
+        {
+                It(should_work_for_basic_tests)
+                {
+                    Assert::That(is_square(-1), Equals(false));
+                    Assert::That(is_square(0), Equals(true));
+                    Assert::That(is_square(3), Equals(false));
+                    Assert::That(is_square(4), Equals(true));
+                    Assert::That(is_square(25), Equals(true));
+                    Assert::That(is_square(26), Equals(false));
+                }
+        };
+
+int main(int argc, char *argv[]) {
+    return TestRunner::RunAllTests(argc, argv);
 }
